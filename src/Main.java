e2d2b780a8d296a9c33696d6ea308faa615bf385
PiGru08 <grucar77@gmail.com>
2024-09-15 16:46:20 +0200

    Krótki program, dane osobowe

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj podaj imię:");
-        Scanner klawiatura = new Scanner(System.in);
-        String imie = klawiatura.nextLine();
+        Scanner klawiaturaImie = new Scanner(System.in);
+        String imie = klawiaturaImie.nextLine();
         System.out.println("Witaj " + imie + "!");
+        System.out.println("Ile masz lat?");
+        int wiek;
+        Scanner klawiaturaWiek = new Scanner(System.in);
+        wiek = klawiaturaWiek.nextInt();
+        wiek = wiek + 10;
+        System.out.println("Twój wiek za 10 lat będzie wynosił: " + wiek + " lat");
+        System.out.println("Czy chciałbyś kontynować? Jeśli tak wpisz 1, jeżeli nie wpisz 0");
+        int dzialanie;
+        Scanner klawiaturaDzialanie = new Scanner(System.in);
+        dzialanie = klawiaturaDzialanie.nextInt();
+        if(dzialanie==0){
+            return;
+        }else{
+            System.out.println("Z jakiego miasta pochodzisz?");
+            String miasto;
+            Scanner klawiaturaMiasto = new Scanner(System.in);
+            miasto = klawiaturaMiasto.nextLine();
+            System.out.println("Twoje dane to: " + imie + ", " + wiek + " lat " + miasto);
+            System.out.println("Program zakończony dzikuję za użcyię ;)");
+            return;
+
+        }
+
     }
 }
