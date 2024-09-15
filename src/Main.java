e36f1a3cdf01a60e46de53482a60a39d9d388c54
PiGru08 <grucar77@gmail.com>
2024-09-15 16:37:13 +0200

    Wypisywanie imienia użytkownika

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Witaj podaj imię:");
+        Scanner klawiatura = new Scanner(System.in);
+        String imie = klawiatura.nextLine();
+        System.out.println("Witaj " + imie + "!");
     }
 }
